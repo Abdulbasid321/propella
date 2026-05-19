0273107c7880ecce51227b4716360d995bcab115
abdulbasid <abdulbasidhussain9@gmail.com>
2026-05-19 14:02:53 -0700

    fix: fix login

--- apps/web/lib/constants.ts
+++ apps/web/lib/constants.ts
@@ -1,3 +1,3 @@
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:5000'
-
-export const API_URL = 'https://propella-uo1q.onrender.com'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:5000'
+// 
+// export const API_URL = 'https://propella-uo1q.onrender.com'
